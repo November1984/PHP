0ea491ed07fbc018d50bad2dd6c372e091e42072
November <november.mcmlxxxiv@icloud.com>
2023-07-24 16:31:18 +0300

    добавил gitignore

---
+++ lesson1/project/index.php
@@ -0,0 +1,6 @@
+<?php
+
+$answer = "Da";
+
+if ( "39попугаев" == var_dump(39) ) {
+    var_dump($answer);    };
--- lesson1/project/script.php
+++ lesson1/project/script.php
@@ -1,14 +1,16 @@
 <?php
 $name = readline("Привет! Как тебя зовут? \n");
 $age = readline("Сколько тебе лет? \n");
-$question_1 = "Какая задача стоит перед вами сегодня?";
-$question_2 = "Сколько примерно времени эта задача займет?";
-$item_1 = (string)readline($question_1);
-$time_1 = (integer)readline($question_2);
-$item_2 = (string)readline($question_1);
-$time_2 = (integer)readline($question_2);
-$item_3 = (string)readline($question_1);
-$time_3 = (integer)readline($question_2);
+
+const Question_1 = "Какая задача стоит перед вами сегодня?";
+const Question_2 = "Сколько примерно времени эта задача займет?";
+
+$item_1 = readline(Question_1);
+$time_1 = (integer)readline(Question_2);
+$item_2 = readline(Question_1);
+$time_2 = (integer)readline(Question_2);
+$item_3 = readline(Question_1);
+$time_3 = (integer)readline(Question_2);
 
 $num = 1;
 $sum = $time_1 + $time_2 + $time_3;
