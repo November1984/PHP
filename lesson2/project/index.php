fc41955aeb2cd71e6e3809b46337a5f681f9a153
November <november.mcmlxxxiv@icloud.com>
2023-07-31 16:28:19 +0300

    доработка урока 1

---
+++ lesson2/project/index.php
@@ -0,0 +1,23 @@
+<?php
+$question = "\nВ каком году произошло крещение Руси?
+a. 810;
+b. 990;
+c. 740;
+d. нет правильного ответа;
+";
+
+while (true) {
+    $answer = (string) readline($question);
+    switch ($answer) {
+        case "d":
+            break(2);
+        case "a":
+        case "b":
+        case "c":
+            echo "\nОтвет не верен\n";
+            break(2);
+        default:
+            echo "\nА? Не понял ответа...\n";
+    }
+}
+echo "Досвидания!";
