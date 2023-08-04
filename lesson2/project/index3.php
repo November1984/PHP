de30b1af7c8aad73922eb42dfec4550c1444788f
November <november.mcmlxxxiv@icloud.com>
2023-08-04 14:57:16 +0300

    Доп. задание урока 2

---
+++ lesson2/project/index3.php
@@ -0,0 +1,25 @@
+<?php
+do {
+    $number = (integer)readline("Укажите число, сударь: ");
+} while ($number <= 0);
+
+    $finger = $number % 9 + (int)($number / 9) % 10;
+
+    switch (true) {
+        case ($finger == 1 or $finger == 0)  :
+            print "Большой";
+            break;
+        case $finger == 2 or $finger == 8 :
+            print "Указательный";
+            break;
+        case $finger == 3 or $finger == 7 :
+            print "Средний";
+            break;
+        case $finger == 4 or $finger == 6 :
+            print "Безымянный";
+            break;
+        case $finger == 5 :
+            print "Мизинец";
+            break;
+    }
+    print "\n";
