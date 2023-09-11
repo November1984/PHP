d14b8993b6bf050ea463d07e486b05f6e9a208bd
November <november.mcmlxxxiv@icloud.com>
2023-09-11 15:28:36 +0300

    ДЗ 1 вроде как готово.

--- lesson5/project/index.php
+++ lesson5/project/index.php
@@ -1,1 +1,19 @@
 <?php
+
+require_once "Task.php";
+require_once "User.php";
+require_once "Time.php";
+
+$user = new User("Семён", "kakoiTo@mail");
+print_r($user);
+$user->addTask("Описание задачи 1");
+print_r($user->getTasks());
+$user->setSex("male");
+$user->addTask("Описание задачи 2");
+
+
+print_r($user);
+print_r($user->getTasks()[1]);
+sleep(5);
+$user->getTasks()[1]->setPriority(2);
+print_r($user->getTasks()[1]);
