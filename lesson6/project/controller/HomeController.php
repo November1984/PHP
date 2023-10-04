5b862b9d92e01c366085dfaea4837971e23f72f2
November <november.mcmlxxxiv@icloud.com>
2023-10-04 17:35:17 +0300

    Изучаю методичку страница 13

---
+++ lesson6/project/controller/HomeController.php
@@ -0,0 +1,3 @@
+<?php
+$pageHeader = "Добро пожаловать!";
+require_once "view/home.php";
--- lesson6/project/index.php
+++ lesson6/project/index.php
@@ -1,31 +1,4 @@
 <?php
-    $teamName = "A";
-    $members = [
-        "Иванов Иван",
-        "Васильев Василий",
-        "Кириллов Кирилл",
-        "Антонов Антон",
-    ];
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Участники команды "<?=$teamName?>"</title>
-</head>
-<body>
-    <h2>Список участников команды "<?=$teamName?>"</h2>
-    <?php
-        function printTeamMembers(array $members): void
-        {
-            echo "<ol>";
-                foreach ($members as $member) {
-                    echo "<li>$member</li>";
-                }
-            echo "</ol>";
-        }
-    ?>
-    <?php printTeamMembers($members);?>
-</body>
-</html>
+
+require_once "controller/HomeController.php";
+
