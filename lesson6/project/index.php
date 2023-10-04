d6a3530a47b25b4e60acee89e493494d7d00b13f
November <november.mcmlxxxiv@icloud.com>
2023-10-04 16:42:05 +0300

    Изучаю методичку 2

--- lesson6/project/index.php
+++ lesson6/project/index.php
@@ -1,32 +1,28 @@
 <?php
-var_dump($_GET);
-    $title = "Наш сайт для php";
-    $pageHeader = "Добро пожаловать!";
-    $userName = "Анатолий";
+    $teamName = "A";
+    $teamMembers = [
+        "Иванов Иван",
+        "Васильев Василий",
+        "Кириллов Кирилл",
+        "Антонов Антон",
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title>Участники команды "<?=$teamName?>"</title>
 </head>
 <body>
-    <h1><?php echo $pageHeader; ?></h1>
-    <?php if ($userName !== null) :?>
-        <h3>Здравствуйте, <?=$userName?></h3>
-    <?php endif; ?>
-
-    <?php if ($userName === 'Василий') : ?>
-        <p>Сообщение для Василия</p>
-     <?php elseif ($userName === 'Анатолий') : ?>
-        <p>Сообщение для Анатолия</p>
-    <?php else : ?>
-        <p>Сообщение для пользователя</p>
-    <?php endif; ?>
+    <h2>Список участников команды "<?=$teamName?>"</h2>
+    <ol>
+        <?php
+            foreach ($teamMembers as $member) :
+                ?>
+                <li><?=$member?></li>
+            <?php endforeach;
+        ?>
+    </ol>
 </body>
 </html>
-
-<?php
-
-?>
