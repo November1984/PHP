b06544c4a31bde3ffa4a641e2ae669d594a09918
November <november.mcmlxxxiv@icloud.com>
2023-10-16 16:30:24 +0300

    Изучаю методичку страница 23

--- lesson6/project/controller/HomeController.php
+++ lesson6/project/controller/HomeController.php
@@ -5,7 +5,8 @@ $userName = isset($_REQUEST['userName']) && !empty($_REQUEST['userName']) ?
             $_REQUEST['userName']: null;
 
 if ($userName !== null) {
-    setcookie('userName', $userName);
+    $expires = time() + 86400;
+    setcookie('userName', $userName, $expires);
 }
 if (isset($_COOKIE['userName'])){
     var_dump($_COOKIE);
