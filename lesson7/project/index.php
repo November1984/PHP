b576d48f8ae187da685314476f4b62735c6d720d
November <november.mcmlxxxiv@icloud.com>
2023-10-31 17:45:10 +0300

    Методика страница 6

--- lesson7/project/index.php
+++ lesson7/project/index.php
@@ -1,5 +1,13 @@
 <?php
 
-$drivers = PDO::getAvailableDrivers();
-var_dump($drivers);
+$pdo = new PDO ("sqlite:database.db");
 
+$query = 'CREATE TABLE `students`(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+
+            name VARCHAR (100) NOT NULL
+        )';
+
+$statement = $pdo->query($query);
+
+var_dump($pdo->errorInfo());
